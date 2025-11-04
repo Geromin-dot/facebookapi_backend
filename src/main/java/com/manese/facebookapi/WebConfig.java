059122a7aa0efc6bed6fa60d43eb524eb4c73a6e
Geromin-dot <grv_manese.student@ua.edu.ph>
2025-11-04 11:50:01 +0800

    Final Push

--- src/main/java/com/manese/facebookapi/WebConfig.java
+++ src/main/java/com/manese/facebookapi/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173") // or "*" for quick test
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
